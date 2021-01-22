6440540f1024515e30b927358bbdb9cca1dfa0d7
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2021-01-22 23:23:03 +0100

    :octocat: token modified

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,8 +1,8 @@
 // const User = require('./../models')
 const jwt = require('jsonwebtoken');
 
-const generateAuthToken = async function (user) {
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET, { expiresIn: '1 Day'} );
+const generateAuthToken = async function ({ _id, fullname, username }) {
+  const token = jwt.sign({ _id, fullname, username  }, process.env.SECRET, { expiresIn: '1 Day'} );
   user.tokens = user.tokens.concat({ token });
 
   await user.save();
