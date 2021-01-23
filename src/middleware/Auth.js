9ef6cd30d8bf31df22df55bfda4f53335385e8f8
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2021-01-23 01:19:38 +0100

    :octocat: auth matter

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -4,13 +4,13 @@ const User = require('./../models/user');
 //adding a route middleware
 const auth = async (req, res, next) => {
 	try {
-		if (!req.headers['cookie']) return res.status(401).send('Access denied. No token provided.');
+		// if (!req.headers['cookie']) return res.status(401).send('Access denied. No token provided.');
 		const token = 
 		req.headers['cookie'].split('=')[1] ||
 		req.header('Authorization').replace('Bearer ', '') ||
 		req.headers['x-access-token'] ||
-		res.headers['x-access-token'];
-
+		res.headers['x-auth-token'];
+		console.log(token);
 		if (!token) return res.status(401).send('Access denied. No token provided.');
 		const decoded = jwt.verify(token, process.env.SECRET);
 		const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
